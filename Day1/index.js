12d8d174f5a69d0a3b7065d4c663540596ba7f8b
vishnu pumar <vishnupumar1996@gmail.com>
2023-03-15 15:13:30 +0530

    day1

---
+++ Day1/index.js
@@ -0,0 +1,10 @@
+// Declaring variable and assigning value
+var message = "Hello, World!"
+
+// printing the message value in the console
+console.log(message)
+
+var name = "Vishnu"
+
+// Here we are concatinating 
+console.log(message + " " + name);
